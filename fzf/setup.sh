92c467c6795212e9cb01dcb9320643626450e3c2
Kevin Greene <kgreenek@gmail.com>
2019-11-29 07:20:47 -0800

    Only clone fzf if not already exists

--- fzf/setup.sh
+++ fzf/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 fzf_install_dir="${HOME}/.fzf"
-#mkdir -p "${fzf_install_dir}/.."
-git clone --depth 1 https://github.com/junegunn/fzf.git "${fzf_install_dir}"
+
+if [[ ! -d $fzf_install_dir ]]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git "${fzf_install_dir}"
+fi
+
 # Don't update rc, because it's already handled in our custom bashrc / zshrc.
 # TODO: Figure out how to always install both bash and zsh.
 ${fzf_install_dir}/install --key-bindings --completion --no-update-rc
