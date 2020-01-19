db33c66b1c20f908559b9a0c351eb7d89c0a8c18
Kevin Greene <kgreenek@gmail.com>
2020-01-19 15:54:31 -0800

    Add install step for Chrome

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -7,6 +7,7 @@ sudo apt-get -qq update
 # highlight enables syntax highlighting in ranger previews.
 # python-dev and python3-dev are needed to compile the youcompleteme vim plugin.
 # vim-gtk enables using the system clipboard from within vim.
+echo "INFO: Installing apt packages..."
 sudo apt-get -qq install -y \
   build-essential \
   caca-utils \
@@ -27,3 +28,12 @@ sudo apt-get -qq install -y \
   time \
   vim-gtk \
   zsh \
+
+# Install chrome
+if [ ! -f "/etc/apt/sources.list.d/google-chrome.list" ]; then
+  echo "INFO: Installing chrome from PPA..."
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+  sudo apt-get -qq update
+  sudo apt-get -qq install -y google-chrome-stable
+fi
