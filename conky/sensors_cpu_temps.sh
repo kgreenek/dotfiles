2e1d01dcdb35bd1629dff6d7206e00153d9ab047
Kevin Greene <kgreenek@gmail.com>
2020-05-30 12:45:47 -0700

    Wrap conky core temps

--- conky/sensors_cpu_temps.sh
+++ conky/sensors_cpu_temps.sh
@@ -4,9 +4,31 @@ if [ -z "$(command -v sensors)" ]; then
   exit 0
 fi
 sensors_output="$(sensors)"
-cores_output="$(echo "${sensors_output}" | grep "^Core" | sed -e 's;\s*(.*);;g')"
-if [ ! -z "${cores_output}" ]; then
-  echo "${cores_output}"
+temps_str="$(echo "${sensors_output}" | grep "^Core")"
+if [ -z "${temps_str}" ]; then
+  temps_str="$("${sensors_output}" | grep -e "^temp" | grep -v "N/A")"
+fi
+if [ -z "${temps_str}" ]; then
+  echo "CPU temps not found"
   exit 0
 fi
-echo "${sensors_output}" | grep -e "^temp" | grep -v "N/A" | sed -e 's;\s*(.*);;g'
+# Remove the extra info from sensors. It looks like this "(high = +100C, ...)"
+temps_str="$(echo "${temps_str}" | sed -e 's;\s*(.*);;g')"
+IFS=$'\n'
+temps=(${temps_str})
+num_lines="${#temps[@]}"
+if [ "${num_lines}" -lt 4 ]; then
+  echo "${temps_str}"
+  exit 0
+fi
+temps_str=""
+num_wrapped_lines=$(((num_lines + 1) / 2))
+for ((i=0; i < num_wrapped_lines; i++)); do
+  right_idx=$((i + num_wrapped_lines))
+  if [ "${right_idx}" -lt "${num_lines}" ]; then
+    temps_str="${temps_str}${temps[${i}]}\t\t${temps[${right_idx}]}\n"
+  else
+    temps_str="${temps_str}${temps[${i}]}\n"
+  fi
+done
+printf "${temps_str}"
