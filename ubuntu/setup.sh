c38d2d934fe126028619db131f5d22aeb10e9836
Kevin Greene <kgreenek@gmail.com>
2021-03-05 09:15:33 -0800

    Add Signal and Docker to ubuntu setup

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -41,6 +41,36 @@ else
   sudo apt-get -qq install -y google-chrome-stable
 fi
 
+# Install signal
+if [ ! -z "$(dpkg -s signal-desktop)" ]; then
+  echo "INFO: Signal already installed. Skipping..."
+else
+  echo "INFO: Installing signal from PPA..."
+  wget -q -O- https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+  echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+  sudo apt-get -qq update
+  sudo apt-get -qq install -y signal-desktop
+fi
+
+# Install docker
+if [ ! -z "$(dpkg -s docker-ce)" ]; then
+  echo "INFO: Docker already installed. Skipping..."
+else
+  echo "INFO: Installing docker from PPA..."
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    | sudo tee -a /etc/apt/sources.list.d/docker.list
+  sudo apt-get -qq update
+  sudo apt-get -qq install -y docker-ce docker-ce-cli containerd.io
+
+  echo "INFO: Creating docker group and adding current user"
+  if [ -z "$(getent group docker)" ]; then
+    sudo groupadd docker
+  fi
+  sudo usermod -aG docker "${USER}"
+fi
+
+# Install nodejs
 if [ -d "${HOME}/.nvm" ]; then
   echo "INFO: nvm / nodejs already installed. Skipping..."
 else
