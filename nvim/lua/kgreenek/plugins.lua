56759cafffa4a63d1917a631dbf283f6a3f8d2a5
Kevin Greene <kgreenek@gmail.com>
2021-12-25 21:45:24 -1000

    Add some performance plugins

--- nvim/lua/kgreenek/plugins.lua
+++ nvim/lua/kgreenek/plugins.lua
@@ -10,7 +10,7 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
     install_path,
   })
   print("Installing packer. Close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
+  vim.cmd("packadd packer.nvim")
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -21,11 +21,17 @@ vim.cmd([[
   augroup end
 ]])
 
+-- This needs to be loaded before any other plugin in order to work.
+pcall(require, "impatient")
+
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
 end
+
+-- TODO(kgreenek): Is this necessary?
+require("packer_compiled")
 
 -- Have packer use a popup window
 packer.init({
@@ -37,48 +43,57 @@ packer.init({
 })
 
 -- Install plugins
-return packer.startup(function(use)
-  use("JamshedVesuna/vim-markdown-preview")
-  use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" })
-  use("bazelbuild/vim-ft-bzl")
-  use("cstrahan/vim-capnp")
-  use("drmikehenry/vim-headerguard")
-  use("folke/which-key.nvim")
-  use("kevinhwang91/rnvimr")
-  use("kyazdani42/nvim-web-devicons")
-  use("lewis6991/gitsigns.nvim")
-  use("nvim-lua/popup.nvim")
-  use("nvim-lua/plenary.nvim")
-  use("nvim-telescope/telescope.nvim") -- Depends: plenary.nvim, nvim-treesitter
-  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-  use("tikhomirov/vim-glsl")
-  use("tpope/vim-fugitive")
-  use("wbthomason/packer.nvim") -- Have packer manage itself
+return packer.startup({
+  function(use)
+    use("JamshedVesuna/vim-markdown-preview")
+    use({ "akinsho/bufferline.nvim", requires = "kyazdani42/nvim-web-devicons" })
+    use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
+    use("bazelbuild/vim-ft-bzl")
+    use("cstrahan/vim-capnp")
+    use("drmikehenry/vim-headerguard")
+    use("folke/which-key.nvim")
+    use("kevinhwang91/rnvimr")
+    use("kyazdani42/nvim-web-devicons")
+    use("lewis6991/gitsigns.nvim")
+    use("lewis6991/impatient.nvim")
+    use("lukas-reineke/indent-blankline.nvim")
+    use("nvim-lua/popup.nvim")
+    use("nvim-lua/plenary.nvim")
+    use("nvim-telescope/telescope.nvim") -- Depends: plenary.nvim, nvim-treesitter
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use("tikhomirov/vim-glsl")
+    use("tpope/vim-fugitive")
+    use("wbthomason/packer.nvim") -- Have packer manage itself
 
-  -- Themes
-  use("folke/tokyonight.nvim")
+    -- Themes
+    use("folke/tokyonight.nvim")
 
-  -- cmp plugins
-  use("hrsh7th/cmp-buffer")
-  use("hrsh7th/cmp-cmdline")
-  use("hrsh7th/cmp-nvim-lsp")
-  use("hrsh7th/cmp-nvim-lua")
-  use("hrsh7th/cmp-path")
-  use("hrsh7th/nvim-cmp") -- The completion plugin
-  use("saadparwaiz1/cmp_luasnip") -- snippet completions
+    -- cmp plugins
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-cmdline")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-nvim-lua")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/nvim-cmp") -- The completion plugin
+    use("saadparwaiz1/cmp_luasnip") -- snippet completions
 
-  -- snippets
-  use("L3MON4D3/LuaSnip") --snippet engine
-  use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+    -- snippets
+    use("L3MON4D3/LuaSnip") --snippet engine
+    use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-  -- LSP
-  use("jose-elias-alvarez/null-ls.nvim")
-  use("neovim/nvim-lspconfig")
-  use("williamboman/nvim-lsp-installer")
+    -- LSP
+    use("jose-elias-alvarez/null-ls.nvim")
+    use("neovim/nvim-lspconfig")
+    use("williamboman/nvim-lsp-installer")
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
-end)
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+      require("packer").sync()
+    end
+  end,
+  config = {
+    -- Move to lua dir so impatient.nvim can cache it.
+    compile_path = vim.fn.stdpath("config") .. "/lua/packer_compiled.lua",
+  },
+})
