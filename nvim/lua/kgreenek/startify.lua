629dc80349e58b826981cec7136ca3decbabc75b
Kevin Greene <kgreenek@gmail.com>
2022-01-25 22:37:27 -0800

    Add lua files for startify and fidget

---
+++ nvim/lua/kgreenek/startify.lua
@@ -0,0 +1,20 @@
+-- Automatically save loaded session on exit.
+vim.g.startify_custom_header = {
+  "                                       ██",
+  "                                      ░░",
+  "    ███████   █████   ██████  ██    ██ ██ ██████████",
+  "   ░░██░░░██ ██░░░██ ██░░░░██░██   ░██░██░░██░░██░░██",
+  "    ░██  ░██░███████░██   ░██░░██ ░██ ░██ ░██ ░██ ░██",
+  "    ░██  ░██░██░░░░ ░██   ░██ ░░████  ░██ ░██ ░██ ░██",
+  "    ███  ░██░░██████░░██████   ░░██   ░██ ███ ░██ ░██",
+  "   ░░░   ░░  ░░░░░░  ░░░░░░     ░░    ░░ ░░░  ░░  ░░",
+}
+vim.g.webdevicons_enable_startify = 1
+vim.g.startify_session_persistence = 1
+vim.g.startify_lists = {
+  { type = "sessions", header = { "   Sessions" } },
+  { type = "files", header = { "   MRU" } },
+  { type = "dir", header = { "   MRU " .. vim.fn.getcwd() } },
+  { type = "bookmarks", header = { "   Bookmarks" } },
+  { type = "commands", header = { "   Commands" } },
+}
