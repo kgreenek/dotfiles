3b3080ff144370880a1054aa0f65f6b6207ea069
Kevin Greene <kgreenek@gmail.com>
2022-11-21 13:18:20 -0800

    Compatibility fixes in handlers.lua

--- nvim/lua/kgreenek/lsp/handlers.lua
+++ nvim/lua/kgreenek/lsp/handlers.lua
@@ -47,7 +47,7 @@ end
 
 M.on_attach = function(client, _)
   if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.document_formatting = false
   end
   lsp_highlight_document(client)
 end
@@ -59,6 +59,6 @@ if not status_ok then
   return
 end
 
-M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 return M
