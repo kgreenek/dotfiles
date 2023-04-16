130829376e2eab8a9a7fe1652efec4891a4e9e19
Kevin Greene <kgreenek@gmail.com>
2023-04-16 10:33:32 -0700

    Fix snap install commands

--- snap/setup.sh
+++ snap/setup.sh
@@ -4,12 +4,10 @@ sudo snap install \
   jsonnet \
   shellcheck
 
-sudo snap install --classic \
-  discord \
-  go \
-  slack \
-  yq
+sudo snap install --classic discord
+sudo snap install --classic go
+sudo snap install --classic slack
+sudo snap install --classic yq
 
 # We need at least v0.8, which is newer than even the neovim PPA.
-sudo snap install --edge --classic \
-  nvim
+sudo snap install --edge --classic nvim
