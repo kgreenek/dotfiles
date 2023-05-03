fa97b335386ea3553b1101e0fe5ecfdd9622da9f
Kevin Greene <kgreenek@gmail.com>
2023-05-03 11:29:43 -0700

    Fix snap setup.sh errors / warnings

--- snap/setup.sh
+++ snap/setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-sudo snap install \
-  btop \
-  jsonnet \
-  shellcheck
+sudo snap install btop
+sudo snap install discord
+sudo snap install jsonnet
+sudo snap install shellcheck
+sudo snap install yq
 
-sudo snap install --classic discord
 sudo snap install --classic go
 sudo snap install --classic slack
-sudo snap install --classic yq
-
 # We need at least v0.8, which is newer than even the neovim PPA.
 sudo snap install --edge --classic nvim
