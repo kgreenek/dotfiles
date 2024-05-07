c0fc6535f82249aab902936a9922874d8f58487e
Kevin Greene <kgreenek@gmail.com>
2024-05-06 20:20:56 -0700

    Fix mise install on first run

--- npm/setup.sh
+++ npm/setup.sh
@@ -5,9 +5,11 @@ if [ -f "${HOME}/.local/bin/mise" ]; then
   echo "INFO: mise already installed. Skipping..."
 else
   curl https://mise.run | sh
-  eval "$("${HOME}/.local/bin/mise activate zsh")"
   "${HOME}/.local/bin/mise" use --global node@latest
 fi
+
+# Ensure mise is avaialable if this is the first time setting up mise.
+export PATH="${HOME}/.local/share/mise/shims:$PATH"
 
 npm install --global yarn
 npm install --no-fund --global neovim
