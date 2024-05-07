59826c18831d40d3919135f623d47eab6194f6ee
Kevin Greene <kgreenek@gmail.com>
2024-05-06 18:46:17 -0700

    Migrate from nvm to mise for node
    This improves terminal startup time dramatically

--- npm/setup.sh
+++ npm/setup.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-# Install nodejs and npm via nvm.
-if [ -d "${HOME}/.nvm" ]; then
-  echo "INFO: nvm / nodejs already installed. Skipping..."
+# Install nodejs and npm via mise.
+if [ -f "${HOME}/.local/bin/mise" ]; then
+  echo "INFO: mise already installed. Skipping..."
 else
-  echo "INFO: Installing nvm..."
-  # Set PROFILE explicitly to prevent it from adding lines to our bashrc.
-  # We do that ourselves in sh_common/rc.
-  curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | PROFILE='/dev/null' bash
-
-  echo "INFO: Installing nodejs..."
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  echo "yarn" > "${NVM_DIR}/default-packages"
-  nvm install node
-  nvm use node
+  curl https://mise.run | sh
+  eval "$("${HOME}/.local/bin/mise activate zsh")"
+  "${HOME}/.local/bin/mise" use --global node@latest
 fi
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+npm install --global yarn
 npm install --no-fund --global neovim
