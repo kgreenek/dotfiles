978be250fae9498d54bfcab7478186e50725f23a
Kevin Greene <kgreenek@gmail.com>
2024-08-22 10:45:12 -0700

    Disable Ubuntu Pro message during apt upgrade

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -4,6 +4,14 @@ set -e
 echo "INFO: Updating apt sources (may take a while)..."
 sudo apt-get -qq update
 
+if [[ ! -f "/etc/apt/apt.conf.d/20apt-esm-hook.conf.bak" ]]; then
+  # Disable the annoying message that Ubuntu prints to try to get you to sign up for their
+  # "Pro" subscription service.
+  echo "INFO: Disabling Ubuntu ESM message on apt upgrade..."
+  sudo dpkg-divert --divert /etc/apt/apt.conf.d/20apt-esm-hook.conf.bak \
+    --rename --local /etc/apt/apt.conf.d/20apt-esm-hook.conf
+fi
+
 # caca-utils gives us the img2txt binary so ranger can generate ascii image previews.
 # highlight enables syntax highlighting in ranger previews.
 # python-dev and python3-dev are needed to compile the youcompleteme vim plugin.
@@ -33,7 +41,7 @@ sudo apt-get -qq install -y \
   vim-gtk \
   xsel \
   yamllint \
-  zsh \
+  zsh
 
 # Install chrome
 if [ -n "$(dpkg -s google-chrome-stable)" ]; then
@@ -63,8 +71,8 @@ if [ -n "$(dpkg -s docker-ce)" ]; then
 else
   echo "INFO: Installing docker from PPA..."
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
-    | sudo tee /etc/apt/sources.list.d/docker.list
+  echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" |
+    sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt-get -qq update
   sudo apt-get -qq install -y docker-ce docker-ce-cli containerd.io
 
@@ -77,7 +85,7 @@ fi
 
 # Install bazel via apt just for up-to-date bash completions. We manage bazel using bazelisk.
 if [ -n "$(dpkg -s bazel)" ]; then
-  echo "INFO: bazel apt package already installed. Skipping..."
+  echo "INFO: Bazel apt package already installed. Skipping..."
 else
   echo "INFO: Installing bazel from PPA..."
   curl -fsSL https://bazel.build/bazel-release.pub.gpg | sudo gpg --dearmor -o /usr/share/keyrings/bazel-archive-keyring.gpg
