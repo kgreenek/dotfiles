0c7c574bdc8135294a709f0c754baa0fb1d089a4
Kevin Greene <kgreenek@gmail.com>
2025-12-02 14:47:03 -0800

    Update comment for neovim snap version

--- snap/setup.sh
+++ snap/setup.sh
@@ -7,5 +7,5 @@ sudo snap install yq
 
 sudo snap install --classic go
 sudo snap install --classic slack
-# We need at least v0.8, which is newer than even the neovim PPA.
+# We need at least v0.11, which is newer than even the neovim PPA.
 sudo snap install --classic nvim
