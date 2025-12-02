7298b8c4088abb363bfd2a29adbf2ef0c0b8e599
Kevin Greene <kgreenek@gmail.com>
2025-12-02 14:30:10 -0800

    Use latest stable snap version of nvim instead of edge

--- snap/setup.sh
+++ snap/setup.sh
@@ -8,4 +8,4 @@ sudo snap install yq
 sudo snap install --classic go
 sudo snap install --classic slack
 # We need at least v0.8, which is newer than even the neovim PPA.
-sudo snap install --edge --classic nvim
+sudo snap install --classic nvim
